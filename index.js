26d98d04af8ce5e2096414ef1965b155194c02f0
Reinier Blanco <reinierblanco10@gmail.com>
2019-05-10 04:09:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 // Add your functions and code here
 
 function destructivelyAppendKitten(kittens) {
-    kittens.pop();
+    kittens.push();
     return kittens;
 }
 
