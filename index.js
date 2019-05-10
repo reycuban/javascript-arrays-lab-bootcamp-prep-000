aef3cfe6e17a43f4bcbb2b58628dd10aeb3db200
Reinier Blanco <reinierblanco10@gmail.com>
2019-05-10 04:21:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,13 +23,13 @@ function destructivelyRemoveFirstKitten() {
 }
 
 function prependKitten(name) {
-  return [name, ...kittens]
+  return [name, ...kittens];
 }
 
  function removeLastKitten() {
-  return kittens.slice(0, kittens.length - 1)
+  return kittens.slice(0, kittens.length - 1);
 }
 
  function removeFirstKitten() {
-  return kittens.slice(1, kittens.length)
+  return kittens.slice(1, kittens.length);
 }
